f0ecaa3f531cb9cdd329f5a786561b3d606af207
SYANG39 <syang39@mgh.harvard.edu>
2022-08-25 14:51:53 +0000

    fix

--- utils/move_files_and_renmae.sh
+++ utils/move_files_and_renmae.sh
@@ -7,3 +7,7 @@ for i in salmon/* ; do
 		is_wrong=1 
 	fi 
 done
+
+awk -F"\t" '{print $1, $2,"\t"$3,"\t"$4,"\t"$5,"\t"$6,"\t"$7,"\t"$8,"\t"$9" gene_type \"$3\";"}' Mmus38.geve.v1.gtf > Mmus38.geve.v1_mod.gtf
+
+grep -v "^##" gencode.vM30.chr_patch_hapl_scaff.annotation.gtf | cut -f9 | sed -n -e 's/^.*gene_type //p' | cut -d';' -f1 | uniq 
