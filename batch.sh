858b6e311390b8dc6ca62bc788764aa14fb24aa3
SYANG39 <syang39@mgh.harvard.edu>
2022-08-26 14:57:16 +0000

    fix

--- batch.sh
+++ batch.sh
@@ -28,6 +28,38 @@ run_batch() {
 	esac
 }
 
+wait_batch() {
+	while [[ ${#my_jobs[@]} -ge $MAX_PARALLEL ]]; do
+		timed_print ${my_jobs[@]}
+		sleep 60
+		for j in ${!my_jobs[@]}; do 
+			output=$(ps -p "${my_jobs[$j]}")
+			if [ ! $? -eq 0 ] ; then
+				echo ${my_jobs[$j]} is not running
+				unset my_jobs[$j] # remove it
+			fi
+		done
+		my_jobs=("${my_jobs[@]}")
+	done
+}
+
+launch_batches() {
+	my_jobs=()
+	local tmp_PAIR_FILE="$PAIR_FILE"
+
+	# group_id=$(ps ax -O pgid | grep main.sh)
+	# trap "kill -SIGINT -- -$group_id" SIGINT
+
+	for i in batches/*; do
+		wait_batch
+
+		run_batch $i
+		# (trap "kill 0" SIGINT ; . $main_loc/analysis.sh) &
+		# (. $main_loc/analysis.sh) &
+	done
+	wait ${my_jobs[@]}
+}
+
 check_quant_sf() {
 	rtn=1
 	for i in /results/salmon/*; do 
@@ -58,6 +90,7 @@ check_core_dump() {
 }
 
 loop_until_finished() {
+	my_jobs=()
 	while check_core_dump; do
 		timed_print "there were core dumped :("
 	done
@@ -76,38 +109,11 @@ main() {
 	batch_op
 	setup_lsf
 
-	my_jobs=()
-	local tmp_PAIR_FILE="$PAIR_FILE"
-
-	# group_id=$(ps ax -O pgid | grep main.sh)
-	# trap "kill -SIGINT -- -$group_id" SIGINT
-
-	for i in batches/*; do
-		while [[ ${#my_jobs[@]} -ge $MAX_PARALLEL ]]; do
-			timed_print ${my_jobs[@]}
-			check_core_dump
-			sleep 10
-			for j in ${!my_jobs[@]}; do 
-				output=$(ps -p "${my_jobs[$j]}")
-				if [ ! $? -eq 0 ] ; then
-					echo ${my_jobs[$j]} is not running
-					unset my_jobs[$j] # remove it
-				fi
-			done
-			my_jobs=("${my_jobs[@]}")
-		done
-
-		run_batch $i
-
-		# (trap "kill 0" SIGINT ; . $main_loc/analysis.sh) &
-		# (. $main_loc/analysis.sh) &
-	done
-
+	launch_batches
 	loop_until_finished
 
 	CHILD="false"
 	PAIR_FILE="$tmp_PAIR_FILE"
-	wait ${my_jobs[@]}
 	rm -r batches
 }
 
