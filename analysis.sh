369dd46d7b7942b2288dafa2d0277844315e0b87
SYANG39 <syang39@mgh.harvard.edu>
2022-08-29 15:12:11 +0000

    fix

--- analysis.sh
+++ analysis.sh
@@ -3,26 +3,36 @@
 
 . $main_loc/timed.sh
 
+# change the source location if it is a relative path
 if [ ! ${SOURCE_LOC[0]} = "/" ]; then
 	SOURCE_LOC="../$SOURCE_LOC"
 fi
+
+# get the file names only and w/o the extensions
 SOURCE=$(basename $SOURCE_LOC)
 REF_GENOME=$(basename $REF_GENOME_LOC .gz) # for salmon decoy
 REF_ANNOTATION=$(basename $REF_ANNOTATION_LOC .gz) # for subread
 REF_TRANSCRIPT=$(basename $REF_TRANSCRIPT_LOC .gz) # for salmon quant, fasta file
 TRANSCRIPTS=$(basename $(basename $TRANSCRIPT_LOCS .gz) .bz2) # for salmon quant, fasta file
 
-id_types="ID"
-
 split_fastq(){ #splits pair ended fastq from bam into 2 files
+	# check the bam file if it is pair ended
+	# get the flags for the file
 	local bam_flags=$(samtools view -c -f 1 "${SOURCE_LOC}/$1.bam")
+	# convert the flags into words
 	local str_flags=$(samtools flags $bam_flags)
+
+	# if they are paired, split the file into the constituent pairs
+	# generate the pairs from the flags for each transcript (either ending in 1 or 2)
+	# we also zip up the files to save space and for consistency
 	if  [[ $str_flags == *"PAIRED"* ]]; then 
 		cat "tmp/${SOURCE}/$1.fq" | grep '^@.*/1$' -A 3 --no-group-separator | \
 				gzip > "tmp/${SOURCE}/$1.r1.fq.gz"
 		cat "tmp/${SOURCE}/$1.fq" | grep '^@.*/2$' -A 3 --no-group-separator | \
 				gzip > "tmp/${SOURCE}/$1.r2.fq.gz"
 		echo -e "$1.r1,$1.r2\n" >> $PAIR_FILE
+
+	#if we should keep single ended files, then we compress the fq and
 	elif [ $EXIT_ON_SINGLE = "false" ]; then 
 		gzip -c tmp/${SOURCE}/$1.fq > tmp/${SOURCE}/$1.fq.gz
 		echo -e "$1,\n" >> $PAIR_FILE 
@@ -57,23 +67,25 @@ group_fastq(){ #group fastq files into pairs
 	files=(tmp/${SOURCE}/*.fq.gz)
 	local total=0
 	for i in "${!files[@]}"; do #loop w/ index bcs easier
-		# local file_name=$(echo "${files[$i]}" | cut -f 1 -d '.') #get file name w/o extention
-		local file_name=${files[$i]%%.*}
-		local file_ext=${files[$i]##*.}
+		# local file_name=$(echo "${files[$i]}" | cut -f 1 -d '.') 
+		local file_name=${files[$i]%%.*} #get file name w/o extention
+		# local file_ext=${files[$i]##*.} # not sure why this is here.. :(
+
 		if ! grep -q "$(basename $file_name .fq.gz)" $PAIR_FILE ; then #if the file does not have pair
 			if check_name ${files[$i]}; then #if formatted correctly
 				echo -e "$(basename $file_name),$(get_r2name $(basename $file_name))" >> $PAIR_FILE
 			elif ! echo "${files[$i]}" | grep -iq "r2" ; then #the choice is yours how to deal with single-ended files
 				timed_print "compliment to ${files[$i]} not found"
 				((total+=1))
-				if [ $EXIT_ON_SINGLE = "false" ]; then 
+
+				if [ $EXIT_ON_SINGLE = "false" ]; then # should single ended files will be included
 					echo -e "$(basename $file_name)," >> $PAIR_FILE
 				fi
 			fi
 		fi
 	done
 
-	if [[ $total -ge 1 && $EXIT_ON_SINGLE = "true" ]]; then 
+	if [[ $total -ge 1 && $EXIT_ON_SINGLE = "true" ]]; then  # if single ended files result in errors
 		exit 1
 	fi
 
@@ -109,21 +121,21 @@ get_pairs_all() { #place all files into tmp, group them
 	group_fastq #groups all other files
 }
 
-fastp_qc(){ #only works for pair ended as of now
+fastp_qc(){ 
+	# results stored in /tmp/{name of the source}/qc/* and all end in *.qc.fq.gz
 	if [ "$OVER_WRITE" = "true" ] || [ ! -f "tmp/${SOURCE}/qc/$1.qc.fq.gz" ]; then
-		# echo "$PWD"
-		if [[ $# -eq 2 ]]; then 
+		if [[ $# -eq 2 ]]; then # if pair ended
 			./fastp -i "tmp/${SOURCE}/$1.fq.gz" -o "tmp/${SOURCE}/qc/$1.qc.fq.gz" \
 				-I "tmp/${SOURCE}/$2.fq.gz" -O "tmp/${SOURCE}/qc/$2.qc.fq.gz" \
 				-j "results/fastp/$1.json" -h "results/fastp/$1.html"
-		elif [[ $# -eq 1 ]]; then
+		elif [[ $# -eq 1 ]]; then # if single ended
 			./fastp -i "tmp/${SOURCE}/$1.fq.gz" -o "tmp/${SOURCE}/qc/$1.qc.fq.gz" \
 				-j "results/fastp/$1.json" -h "results/fastp/$1.html"
 		fi	
 	fi
 }
 
-qc_all(){
+qc_all(){ # only supports fastp as of now
 	if [[ ! -d "tmp/${SOURCE}/qc" ]]; then 
 		mkdir "tmp/${SOURCE}/qc"
 	fi
@@ -133,8 +145,10 @@ qc_all(){
 	fi
 
 	timed_print "qc-ing with $QC_METHOD"
-	while IFS=, read -r r1 r2; do
-		if [ ! -d "results/${ALIGN_METHOD}/$r1" ]; then
+	while IFS=, read -r r1 r2; do # loop over pair file, csv
+		if [ "$OVER_WRITE" = "true" ] || [ ! -d "results/${ALIGN_METHOD}/$r1" ]; then 
+			#if salmon quant already exists
+
 			timed_print "qc-ing ${r1} and ${r2}"
 			case $QC_METHOD in 
 				fastp) fastp_qc $r1 $r2 ;;
@@ -152,22 +166,28 @@ subread_build_index(){
 	fi
 }
 
-salmon_build_index(){
-	local index_name="salmon/$(IFS=-; echo "${TRANSCRIPTS[*]%.*}")"
+salmon_build_index(){ # supports multiple transcripts
+	# get dir name for the index, which is the transcript names seperated by a hyphen(-)
+	# eg. for erv.fa and line.fa, you get: erv-line
+	local index_name="salmon/$(IFS=-; echo "${TRANSCRIPTS[*]%.*}")" 
 	if [ "$OVER_WRITE" = "true" ] || [ ! -d "${index_name}_index" ]; then 
 		timed_print "building salmon index @: ${index_name}_index"
 
+		# salmon requires a decoy, which in this case is the entire reference genome
 		if [ "$OVER_WRITE" = "true" ] || [ ! -f "${index_name}_decoys.txt" ]; then
 			timed_print "building decoys..."
 			grep "^>" "${REF_GENOME}" | cut -d " " -f 1 > "${index_name}_decoys.txt"
 			sed -i.bak -e 's/>//g' "${index_name}_decoys.txt"
 		fi 
 
+		# salmon requires all the transcripts being quantified to be concat into one file
 		if [ "$OVER_WRITE" = "true" ] || [ ! -f "${index_name}_gentrome.fa" ]; then
 			timed_print "building gentrome..."
 			cat "${TRANSCRIPTS[@]}" "${REF_TRANSCRIPT}" "${REF_GENOME}" > "${index_name}.fa"
 		fi
 
+		# adding --gencode flag due to salmon doing some preprocessing for it
+		# this flag does nothing for none gencode files (I think)
 		salmon index -t ${index_name}.fa -d "${index_name}_decoys.txt" -i "${index_name}_index" --gencode
 	fi
 }
@@ -199,7 +219,7 @@ align_all(){
 	while IFS=, read -r r1 r2; do
 		case $ALIGN_METHOD in 
 			subread) subread_align $r1 $r2 ;;
-			salmon) return 0 ;;
+			salmon) return 0 ;; # salmon does not need alignment, so it does nothing at this step
 		esac
 	done < $PAIR_FILE
 }
@@ -211,7 +231,7 @@ get_gene_types_comb(){
 subread_count_comb(){
 	if [ "$OVER_WRITE" = "true" ] || [ ! -f "results/subread/fc_comb_$1.tsv" ]; then 
 		timed_print "counting $1.bam..."
-		featureCounts -a ${COMB_ANNOTATION}.gff3 -o "results/subread/fc_comb_$1.tsv" "tmp/${SOURCE}/subread_aligned/$1.bam" -T 6 -t ${gene_types[@]} -g ${id_types[@]}
+		featureCounts -a ${COMB_ANNOTATION}.gff3 -o "results/subread/fc_comb_$1.tsv" "tmp/${SOURCE}/subread_aligned/$1.bam" -T 6 -t ${gene_types[@]} -g "ID"
 		timed_print "counted $1.bam"
 	fi
 }
@@ -252,12 +272,15 @@ subread_count() {
 }
 
 salmon_quant() {
+	# get the same index_name as specified in "salmon_build_index()"
 	local index_name="salmon/$(IFS=-; echo "${TRANSCRIPTS[*]%.*}")"
+	#salmon outputs into a dir, so we check for that instead
+	#results stored in results/salmon/*
 	if [ "$OVER_WRITE" = "true" ] || [ ! -d "results/salmon/$1" ]; then
 		mkdir results/salmon/$1
-		if [[ $# -eq 2 ]]; then
+		if [[ $# -eq 2 ]]; then # if pair ended
 			salmon quant -i "${index_name}_index" -l A -1 "tmp/${SOURCE}/qc/$1.qc.fq.gz" -2 "tmp/${SOURCE}/qc/$2.qc.fq.gz" --validateMappings -o "results/salmon/$1"
-		elif [[ $# -eq 1 ]]; then 
+		elif [[ $# -eq 1 ]]; then # if single ended
 			salmon quant -i "${index_name}_index" -l A -r "tmp/${SOURCE}/qc/$1.qc.fq.gz" --validateMappings -o "results/salmon/$1"
 		fi 
 	fi
@@ -284,27 +307,26 @@ main(){
 	timed_print "starting $$"
 	timed_print "analysing..."
 
-	if [[ ! -d "tmp" ]]; then 
-		mkdir tmp
-	fi
-
-	if [[ ! -d "$ALIGN_METHOD" ]]; then
-		mkdir $ALIGN_METHOD 
-	fi
-
-	if [[ ! -d "results" ]]; then 
-		mkdir results
-	fi
+	# make these essential dirs
+	local directories=( "tmp" "$ALIGN_METHOD" "results" )
+	for i in ${directories[@]}; do
+		if [[ ! -d "$i" ]]; then 
+			mkdir $i
+		fi
+	}
 
 	if [ "$ANALYSIS_STEP" = "all" ]; then 
 		ANALYSIS_STEP="index,convert,qc,align,count"
 	fi 
 
-	ANALYSIS_STEP=(${ANALYSIS_STEP//,/ }) #turn "index,c,q" into ( "index" "c" "q" )
+	#turn "index,c,q" into ( "index" "c" "q" )
+	ANALYSIS_STEP=(${ANALYSIS_STEP//,/ }) 
+
 	for i in ${ANALYSIS_STEP[@]}; do
 	  timed_print "$i-ing..."	
 		case "$i" in 
-			index) if [ $CHILD = false ]; then build_index; fi ;;
+			# for the first 2, do not run if they are in a child/forked process
+			index) if [ $CHILD = false ]; then build_index; fi ;; 
 			convert) if [ $CHILD = false ]; then get_pairs_all; fi ;;
 			qc) qc_all ;;
 			align) align_all ;;
@@ -324,7 +346,8 @@ main(){
 			fi
 		done < $PAIR_FILE 
 	fi
-	# usage=$(awk '{u=$2+$4; t=$2+$4+$5; if (NR==1){u1=u; t1=t;} else print ($2+$4-u1) * 100 / (t-t1) "%"; }' <(grep 'cpu ' /proc/stat) <(sleep 0.5;grep 'cpu ' /proc/stat))
+
+	# get the uage metrics before exiting
 	timed_print "exiting $$ with: "
 	top -b -n 1 -u $USER
 	/usr/local/bin/pan_quota $HOME
@@ -350,3 +373,5 @@ main
 
 #gffcompare -R -r gencode.v41.chr_patch_hapl_scaff.annotation.gtf -o siyi_link/combine_annotation/herv_annotation erv.gff3 >>warnings.txt 2>&1
 #cuffmerge -g gencode.v41.chr_patch_hapl_scaff.annotation.gtf -s GRCh38.p13.genome.fa -o siyi_link/combine_annotation/herv_annotation gff_list.txt
+
+# usage=$(awk '{u=$2+$4; t=$2+$4+$5; if (NR==1){u1=u; t1=t;} else print ($2+$4-u1) * 100 / (t-t1) "%"; }' <(grep 'cpu ' /proc/stat) <(sleep 0.5;grep 'cpu ' /proc/stat))
