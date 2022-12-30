005aec28d395b18aec4af238539abd2d4567b019
wangLabMGH <jliu78@mgh.harvard.edu>
2022-12-29 23:44:17 -0500

    fix typo

--- git_download.sh
+++ git_download.sh
@@ -6,6 +6,7 @@ commit_message="fix"
 while [ -n "$1" ]; do #setting variables for sub-processes
 	case "$1" in
 		-from_branch_name) from_branch_name="$2"; shift ;;
+    -f) from_branch_name="$2"; shift ;;
 		-commit_message) commit_message="$2"; shift ;;
 		*) echo "$1 is not an option"; exit 1 ;;
 	esac
